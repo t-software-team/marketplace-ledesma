47b9f3e4323d0a12c0526a71541af8657a71d8ed
Christian Toscano <chrsitiantoscano@MacBook-Pro-de-Christian.local>
2026-08-10 03:12:52 -0300

    add speedinsights

--- marketplace-comercios/src/app/layout.tsx
+++ marketplace-comercios/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { IBM_Plex_Mono, Inter, Sora } from 'next/font/google'
 import { QueryProvider } from '@/components/providers/query-provider'
 import { ThemeProvider } from '@/components/providers/theme-provider'
@@ -85,6 +86,7 @@ export default function RootLayout({
           </Toaster>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
